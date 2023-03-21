982542e6a2f5d1544bf31fea7abfe13e6344db9f
Faith <faithchemutai2330@gmail.com>
2023-03-21 20:55:50 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,12 @@ restaurant = Restaurant.create(name: Faker::Restaurant.name, address: "69 W 55th
 # create some pizzas
 cheese = Pizza.create(name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
 pepperoni = Pizza.create(name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
-veggie = Pizza.create(name: "veggie", ingredients: "Dough, Tomato Sauce, Cheese, Veggies")
-meatpizza = Pizza.create(name: "Meatpizza", ingredients: "Dough, Tomato Sauce, Cheese, Meatpizza")
-margheritta = Pizza.create(name: "Margheritta", ingredients: "Dough, Tomato Sauce, Cheese, Margheritta")
-bbqpizza = Pizza.create(name: "Bbqpizza", ingredients: "Dough, Tomato Sauce, Cheese, Bbqpizza")
-hawaiian = Pizza.create(name: "Hawaiian", ingredients: "Dough, Tomato Sauce, Cheese, Hawaiian")
-buffalopizza = Pizza.create(name: "Buffalopizza", ingredients: "Dough, Tomato Sauce, Cheese, Buffalopizza")
+# veggie = Pizza.create(name: "veggie", ingredients: "Dough, Tomato Sauce, Cheese, Veggies")
+# meatpizza = Pizza.create(name: "Meatpizza", ingredients: "Dough, Tomato Sauce, Cheese, Meatpizza")
+# margheritta = Pizza.create(name: "Margheritta", ingredients: "Dough, Tomato Sauce, Cheese, Margheritta")
+# bbqpizza = Pizza.create(name: "Bbqpizza", ingredients: "Dough, Tomato Sauce, Cheese, Bbqpizza")
+# hawaiian = Pizza.create(name: "Hawaiian", ingredients: "Dough, Tomato Sauce, Cheese, Hawaiian")
+# buffalopizza = Pizza.create(name: "Buffalopizza", ingredients: "Dough, Tomato Sauce, Cheese, Buffalopizza")
 
 
 
